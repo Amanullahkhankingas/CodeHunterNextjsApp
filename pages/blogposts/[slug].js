82282a395002c0f4e5a5b70ebc94f4159b625912
Aman Ullah <84645417+Amanullahkhankingas@users.noreply.github.com>
2022-08-09 22:33:57 +0500

    lecture 20 'Fetching BlogPost using _getblog EndPoint'

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -1,22 +1,45 @@
-import React from 'react'
+// http://localhost:3000/blogposts/how_to_learn_reactjs
+
+import React, { useEffect,useState } from 'react'
 import {useRouter} from  'next/router'
 import styles from '../../styles/BlogPosts.module.css'
+
 
 //Step 1: Find the file corresponding to the slug
 // Step 2: Populate them inside the page
 
 const slug = () => {
 const router = useRouter();
-const {slug} = router.query;
 // console.log(router.query)
 
-  // return <div>{slug} </div>
+const [blog,setBlog]=useState()
+
+// console.log("router is outside ",router.isReady)
+
+useEffect(()=>{
+  // console.log("router is inside ",router.isReady)
+
+  console.log('useeffect is working in blogposts slugs')
+
+  if(!router.isReady) return;
+  
+      const {slug} = router.query;
+
+      fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((titlefiledata=>{
+        
+        return titlefiledata.json();
+      })).then((blogpost)=>{
+        setBlog(blogpost)
+      })
+
+
+  },[router.isReady])
 
   return <div className={styles.container}>
      <div className={styles.container}>
-      <h2>the title of the page: {slug}</h2>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam id quibusdam quaerat laudantium quo provident odit atque tenetur recusandae dolores, voluptate minus laboriosam eaque suscipit doloremque esse, labore similique? Inventore aut non, temporibus dicta ullam ratione aspernatur esse perferendis, velit ducimus ab fuga nesciunt voluptatem enim quos. Tempore ab reprehenderit iste ducimus aliquid id!</p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam id quibusdam quaerat laudantium quo provident odit atque tenetur recusandae dolores, voluptate minus laboriosam eaque suscipit doloremque esse, labore similique? Inventore aut non, temporibus dicta ullam ratione aspernatur esse perferendis, velit ducimus ab fuga nesciunt voluptatem enim quos. Tempore ab reprehenderit iste ducimus aliquid id!</p>
+      <h2>{blog && blog.title}</h2>
+      <p>{blog && blog.content}</p>
+      {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam id quibusdam quaerat laudantium quo provident odit atque tenetur recusandae dolores, voluptate minus laboriosam eaque suscipit doloremque esse, labore similique? Inventore aut non, temporibus dicta ullam ratione aspernatur esse perferendis, velit ducimus ab fuga nesciunt voluptatem enim quos. Tempore ab reprehenderit iste ducimus aliquid id!</p> */}
      </div>
          
          
