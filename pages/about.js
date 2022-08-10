90508d42e7e40a710881b962cfeb1a3354e19936
Aman Ullah <84645417+Amanullahkhankingas@users.noreply.github.com>
2022-08-11 02:12:25 +0500

    lceture 27 'Designing the About Page - Nextjs'

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import styles from '../styles/AboutUs.module.css'
 
 const about = () => {
   return (
-    <div>
-      this is the about page
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Code Hunter</h1>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deserunt porro enim temporibus esse delectus maxime quaerat in ullam, cumque ex debitis voluptatum voluptate, vero, itaque magnam? Voluptas, ullam iure saepe impedit accusantium quas!amet consectetur adipisicing elit. Dicta deserunt porro enim temporibus esse delectus maxime quaerat in ullam, cumque ex debitis voluptatum voluptate, vero, itaque magnam? Voluptas, ullam iure saepe impedit accusantium quas!</p>
+      <h2>Our Services Provided</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deserunt porro enim temporibus esse delectus maxime quaerat in ullam, cumque ex debitis voluptatum voluptate, vero, itaque magnam? Voluptas, ullam iure saepe impedit accusantium quas!amet consectetur adipisicing elit. Dicta deserunt porro enim temporibus esse delectus maxime quaerat in ullam, cumque ex debitis voluptatum voluptate, vero, itaque magnam? Voluptas, ullam iure saepe impedit accusantium quas!</p>
+      <p>We offer the following services:</p>
+      <ul>
+        <li>service 1</li>
+        <li>service 1</li>
+        <li>service 1</li>
+        <li>service 1</li>
+        <li>service 1</li>
+        <li>service 1</li>
+        <li>service 1</li>
+        <li>service 1</li>
+      </ul>
+      <h2>Contact Us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta deserunt porro enim temporibus esse delectus maxime quaerat in ullam, cumque ex debitis voluptatum voluptate, vero, itaque magnam? Voluptas, ullam iure saepe impedit accusantium quas!amet consectetur adipisicing elit. Dicta deserunt porro enim temporibus esse delectus maxime quaerat in ullam, cumque ex debitis voluptatum voluptate, vero, itaque magnam? Voluptas, ullam iure saepe impedit accusantium quas!</p>
     </div>
+
   )
 }
 
